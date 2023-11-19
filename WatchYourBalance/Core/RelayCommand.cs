14b4b47b5c8f3bda318e6b292853c40f038a48ff
Egor <Egor@DESKTOP-10HPMU9>
2023-11-19 16:18:14 +0700

    Добавьте файлы проекта.

---
+++ WatchYourBalance/Core/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WatchYourBalance.Core
+{
+    internal class RelayCommand : ICommand // позволяет связать элементы пользовательского интерфейса (кнопки, меню) с методами в коде приложения.
+    {
+        private Action<object> _execute; // представляет метод, который будет выполнен при вызове команды
+        private Func<object, bool> _canExecute; //представляет метод, который определяет, можно ли выполнить команду в текущем состоянии приложения.
+
+        public event EventHandler CanExecuteChanged // какие то команды ретрансляции
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        //передаются методы, которые будут выполняться при вызове команды и определении ее доступности.
+        //Если метод определения доступности не передан, он принимает значение null, что означает, что команда доступна всегда.
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) //Указывает, может ли команда быть выполнена в данный момент.
+                                                 //Если значение true, команда доступна для выполнения, если false, то нет
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter) // Выполняет команду, то есть вызывает метод, который был передан в конструктор класса RelayCommand.
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ WatchYourBalance/ViewModels/MainVM.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WatchYourBalance.Core;
+using WatchYourBalance.Models;
+
+namespace WatchYourBalance.ViewModels
+{
+    class MainVM : ObservableObject
+    {
+        public ICommand UpdateBalanceCommand { get; private set; }
+
+        public MainVM()
+        {
+            UpdateBalanceCommand = new RelayCommand(async _ => await UpdateBalanceAsync());
+        }
+
+        private string _APISecret;
+        public string APISecret
+        {
+            get { return _APISecret; }
+            set
+            {
+                _APISecret = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _APIKey;
+        public string APIKey
+        {
+            get { return _APIKey; }
+            set
+            {
+                _APIKey = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private decimal _balance;
+        public decimal Balance
+        {
+            get { return _balance; }
+            set
+            {
+                _balance = value;
+                OnPropertyChanged(nameof(Balance));
+            }
+        }
+
+        public async Task UpdateBalanceAsync()
+        {
+            ApiBinance aPIKeys = new ApiBinance();
+            decimal newBalance = await aPIKeys.GetBalance(_APIKey, _APISecret);
+            Balance = newBalance;
+        }
+    }
+}
